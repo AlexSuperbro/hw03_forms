7ae54f83478f5685ebab00a1080a47ad9e2848f9
Александр Нестерихин <NesterAE@mail.ru>
2022-05-23 00:30:05 +0300

    First commit: flake8, mypy, pytests compleated

---
+++ yatube/core/context_processors/year.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+def year(request):
+    """Добавляет переменную с текущим годом."""
+    current_datetime = datetime.now()
+    return {
+        'year': current_datetime.year,
+    }
